dedbc18435c17cc22bd8e435aa730cd9ac55e84e
delar <emmariodelar@gmail.com>
2022-11-18 14:01:17 +0100

    Provide new pcap

--- grc/readPcap.py
+++ grc/readPcap.py
@@ -1,11 +1,13 @@
 from scapy.all import *
 from deep_translator import GoogleTranslator
+from pprint import pprint
 import time
 import io
 import os
 import magic
 import pprint
 import pyshark
+import json
 
 
 global rm
@@ -19,22 +21,25 @@ ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 def remove():
     if rm:
         rm=False
-        os.system('echo > /tmp/in.pcap')
+        os.system('echo > '+ ROOT_DIR + "/wpan.pcap")
         return False
     return True
 
 def translate(line):
-    res=line.decode('gbk', 'ignore')
+    res=line.decode('utf-16be', 'ignore').lower()
     res=re.sub('([^\u0020-\u007E]+)', ' \g<1> ', res)
-    res=re.sub('\s+', ' ', res)
+    res=re.sub('\s{2,}', '', res)
     print('x'*35)
     time.sleep(0.6)
-    print(res)
+    #print(res)
     res=str(yandex.translate(res))
     res=re.sub('é|è|ê', 'e', res)
     res=re.sub('â|à', 'a', res)
-    res=res.encode('gbk', 'ignore')
-    res=res.decode('utf-8', 'ignore')
+    res=re.sub('ô|ö', 'o', res)
+    res=re.sub('î|ï', 'i', res)
+    res=re.sub('û|ü', 'u', res)
+    res=res.encode('utf-16be', 'ignore')
+    res=str(res.decode('utf-8', 'ignore'))
 
     print(res)
     hexdump(line.decode('utf-8', 'ignore')+res)
@@ -43,17 +48,6 @@ mime = magic.Magic(mime=True)
 while 1:
     try:
         pkts=rdpcap(ROOT_DIR + "/wpan.pcap")
-        import json
-        from pprint import pprint
-        """sessions = pkts.sessions()
-        for session in sessions:
-            http_payload = ""
-            for packet in sessions[session]:
-                try:
-                    if packet[TCP].dport == 80 or packet[TCP].sport == 80:
-                        print(packet[TCP].payload)
-                except:
-                    pass"""
         for p in pkts:
             print(p.summary())
             if p:
@@ -62,7 +56,7 @@ while 1:
                     pkt_payload = bytes(p.payload.payload)
                     stype=mime.from_buffer(pkt_payload)
                     print(stype)
-                    print(translate(pkt_payload))
+                    translate(pkt_payload)
                     print('='*35)
         rm=True
         remove()
@@ -75,7 +69,7 @@ while 1:
             rm=True
             remove()
         if "No such":
-            os.system('touch /tmp/in.pcap')
+            os.system('touch ' + ROOT_DIR + "/wpan.pcap")
         print(e)
         #remove(rm)
         pass
